3d9535e92eb6fa70175384a259a7e9744d30ceca
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-03-10 16:50:17 -0800

    new commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,8 @@
  * Return:1
  */
 int main(void)
-
 {
-
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, ch, sizeof(ch));
-
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
 	return (1);
-
 }
