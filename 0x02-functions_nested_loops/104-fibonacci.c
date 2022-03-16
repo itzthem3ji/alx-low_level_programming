4d695968b864cf348b7795c07fadb84430ea776d
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-03-16 02:47:06 +0100

    functions and nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: computes and prints the sum of all the multiples of 3 or
- * 5 below 1024 (excluded), followed by a new line
+ * Description: finds and prints the first 98 Fibonacci numbers,
+ * starting with 1 and 2, followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	int i = 1;
+	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (i < 98)
+	while (i < 96)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 97)
+		if (i < 95)
 			printf(", ");
 		i++;
 	}
