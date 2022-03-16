11a8b5c54cd142dff9179d1b3f102d56e6a95e66
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-03-16 02:32:10 +0100

    functions and nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@
 void print_times_table(int n)
 {
 
-int a = 0, rep, b;
+int a = 0, prodct, b;
 
 if (n < 0 || n > 15)
 	return;
@@ -18,26 +18,26 @@ while (a <= n)
 {
 	for (b = 0; b <= n; b++)
 	{
-		rep = a * b;
+		prodct = a * b;
 		if (b == 0)
-			_putchar('0' + rep);
-		else if (rep < 10)
+			_putchar('0' + prodct);
+		else if (prodct < 10)
 		{
 			_putchar(' ');
 			_putchar(' ');
-			_putchar('0' + rep);
+			_putchar('0' + prodct);
 		}
-		else if (rep < 100)
+		else if (prodct < 100)
 		{
 			_putchar(' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
+			_putchar('0' + prodct / 10);
+			_putchar('0' + prodct % 10);
 		}
 		else
 		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
+			_putchar('0' + prodct / 100);
+			_putchar('0' + (prodct - 100) / 10);
+			_putchar('0' + prodct % 10);
 		}
 		if (b < n)
 		{
