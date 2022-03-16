c47aaf98bde5aadadaaa684b2ea314ae7aa0e4f0
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-03-16 02:56:04 +0100

    functions and nested loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-	int i = 7;
+	int i = 0;
 	unsigned long int a = 0, b = 1, next = 0;
 
-	while (i < 98)
+	while (i < 93)
 	{
 		next = a + b;
 		a = b;
 		b = next;
 		printf("%lu", next);
 
-		if (i < 97)
+		if (i < 92)
 			printf(", ");
 		i++;
 	}
