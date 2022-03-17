807b62bc584710d3783f560ce14fca4b8d0a750d
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-03-17 23:50:49 +0100

    More functions and tested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,20 +7,32 @@
  */
 int main(void)
 {
+	long int n;
+	long int max;
+	long int i;
 
-unsigned int long n = 612852475143, a = (int) sqrt(n);
+	n = 612852475143;
+	max = -1;
 
-while (1)
-{
-
-	if (n % a == 0)
+	while (n % 2 == 0)
 	{
-		printf("%lu \n", n / a);
-		break;
+		max = 2;
+		n /= 2;
 	}
-	a--;
 
-}
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
+	}
 
-return (0);
+	if (n > 2)
+		max = n;
+
+	printf("%ld\n", max);
+
+	return (0);
 }
