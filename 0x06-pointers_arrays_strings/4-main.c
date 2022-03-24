a89e2b19bc22d2351611b9dc0fd9f62b50a796ec
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-03-24 14:29:57 +0100

    Pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-main.c
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the codeSchool students.
  * @a: an array of integers
  * @n: the number of elements to swap
  *
@@ -26,7 +26,7 @@ void print_array(int *a, int n)
 }
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the codeSchool students.
  *
  * Return: Always 0.
  */
