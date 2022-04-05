51f2471bcdb49a777e6b1c5db3eccfad7074ede4
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-04-05 20:27:20 +0100

    Creating arrays

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,10 @@ char *create_array(unsigned int size, char c)
     unsigned int i;
 
     if (size == 0)
-        return NULL;
+        return (NULL);
     ptr = malloc((size) * sizeof(char));
     if (ptr == NULL)
-        return NULL;
+        return (NULL);
     for (i = 0; i < size; i++)
     {
         *(ptr + i) = c;
