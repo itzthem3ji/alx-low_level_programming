767e33b66c5901d82d1d4ad07a23da35bf12e390
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-04-12 05:29:49 +0100

    struct dog

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,5 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,3 +13,4 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
+#endif
