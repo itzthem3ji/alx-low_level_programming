4d37b722f103756172faf3a22cc17668338eadd1
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-05-03 04:49:05 +0100

    file input and output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,30 +10,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int i, y;
+	ssize_t i, y;
 	char *bufr;
+
 	if (!filename)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+
+	if (fd == -1)
 		return (0);
-	bufr = malloc(sizeof(char) * letters);
+
+	bufr = malloc(sizeof(char) * (letters));
 	if (!bufr)
 		return (0);
+
 	i = read(fd, bufr, letters);
-	if (i < 0)
-	{
-		free(bufr);
-		return (0);
-	}
-	bufr[i] = '\0';
-	close(fd);
 	y = write(STDOUT_FILENO, bufr, i);
-	if (y < 0)
-	{
-		free(bufr);
-		return (0);
-	}
+
+	close(fd);
+
 	free(bufr);
+
 	return (y);
 }
