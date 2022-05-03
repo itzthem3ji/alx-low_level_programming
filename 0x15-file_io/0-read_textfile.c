c2782a29a527cdda686d71fa2091294b10696292
Feyitimi Victor <victorfeyitimi@gmail.com>
2022-05-03 04:46:49 +0100

    file input and output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,39 @@
 #include "main.h"
+
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
- * Return: the number of letters printed, or 0 if it failed
+ * read_textfile - reads a text file and prints the letters
+ * @filename: filename.
+ * @letters: numbers of letters printed.
+ *
+ * Return: numbers of letters printed. It fails, returns 0.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	int i, y;
-	char *buf;
+	char *bufr;
 	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+	bufr = malloc(sizeof(char) * letters);
+	if (!bufr)
 		return (0);
-	i = read(fd, buf, letters);
+	i = read(fd, bufr, letters);
 	if (i < 0)
 	{
-		free(buf);
+		free(bufr);
 		return (0);
 	}
-	buf[i] = '\0';
+	bufr[i] = '\0';
 	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
+	y = write(STDOUT_FILENO, bufr, i);
 	if (y < 0)
 	{
-		free(buf);
+		free(bufr);
 		return (0);
 	}
-	free(buf);
+	free(bufr);
 	return (y);
 }
